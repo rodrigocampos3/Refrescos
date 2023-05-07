79eb065e79aa09ae3623129ca3dc12e244cb36bd
rodrigorodriguesb <rodrigo.rodrigues@sou.inteli.edu.br>
2023-05-07 02:54:30 -0300

    my commit

--- Front/front-challenge/pages/cadastro/index.tsx
+++ Front/front-challenge/pages/cadastro/index.tsx
@@ -1,40 +1,84 @@
-import { Layout } from '../../components/Layout'
-import Image from 'next/image'
-import inicial from '../assets/inicial.svg'
-import Button2 from '../../components/Button2'
-import Input from '../../components/input'
+// Importa o hook useState do React, o componente Layout do diretório ../components, a biblioteca Image do Next.js, o componente Button2 do diretório ../components, o componente Input do diretório ../components e a biblioteca axios
+import { useState } from 'react';
+import { Layout } from '../../components/Layout';
+import Image from 'next/image';
+import Button2 from '../../components/Button2';
+import Input from '../../components/input';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
-
+// Define a função Home
 const Home = () => {
-    return (
-        <Layout title={'Home'}>
-            <div className='flex flex-col flex-1 items-center text-center h-full w-full bg-[0F0F0F]'>
-                <div className='mt-[15%] w-full px-6'>
-                    <div className=' text-[#fff] text-md mx-auto px-7 text-center'>
-                        <h3 >Cadastre-se</h3>
-                    </div>
-                    <Input  type="nome" placeholder="Nome" />
-                    <Input type="cpf" placeholder="CPF" />
-                    <Input type="e-mail" placeholder="Digite seu Email" />
-                    <Input type="password" placeholder="Senha" />
-                    <Input type="password" placeholder="Confirmar senha" />
-                </div>
-                <div className='mt-10 text-[#8FA2B7] text-sm mx-7 px-2 text-left'>
-                    <p>Ao prosseguir, você concorda com estes <span className='text-[#468C19]'>Termos e Condições.</span> </p>
-                </div>
-                <div className='w-full px-10 mt-10'>
-                <Button2 text='Create a New Account' />
-                    
-                </div>
-                
 
+    const router = useRouter();
 
+  const handleRedirect = () => {
+    router.push('../cadastro');
+  };
+  
+  // Define quatro estados para armazenar o valor dos campos name, cpf, email e password. Inicialmente, eles estão vazios
+  const [name, setName] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  // Define quatro funções para atualizar os estados quando os campos correspondentes forem modificados
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+  
+  const handleCpfChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCpf(event.target.value);
+  };
 
-            </div>
-        </Layout>
-    )
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
 
-}
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
 
-export default Home
+  // Define a função handleRegister, que será executada quando o botão de registro for clicado. Essa função envia uma requisição POST para o servidor com os dados do formulário
+  const handleRegister = () => {
+    const data = {
+      name,
+      cpf,
+      email,
+      password,
+    };
+    console.log(data)
+    axios.post('http://localhost:3000/register', data)
+      .then(response => {
+        alert('Registro realizado com sucesso!');
+      })
+      .catch(error => {
+        console.error(error);
+        alert('Ocorreu um erro durante o registro.');
+      });
+  };
+
+  return (
+    <Layout title={'Home'}>
+      <div className='flex flex-col flex-1 items-center text-center h-full w-full bg-[black]'>
+        <div className='mt-[15%] w-full px-6'>
+          <div className=' text-[#fff] text-md mx-auto px-7 text-center'>
+            <h3>Cadastre-se</h3>
+          </div>
+          <Input name='nome' type="text" placeholder="Nome" value={name} onChange={handleNameChange} />
+          <Input name='cpf' type="text" placeholder="CPF" value={cpf} onChange={handleCpfChange} />
+          <Input name='email' type="email" placeholder="Digite seu Email" value={email} onChange={handleEmailChange} />
+          <Input name='senha' type="password" placeholder="Senha" value={password} onChange={handlePasswordChange} />
+        </div>
+        <div className='mt-10 text-[#8FA2B7] text-sm mx-7 px-2 text-left'>
+          <p>Ao prosseguir, você concorda com estes <span className='text-[#468C19]'>Termos e Condições.</span> </p>
+        </div>
+        <div className='w-full px-10 mt-10'>
+          <Button2 text='Criar conta' onClick={handleRegister} backgroundColor='bg-[#468C19]' />
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
--- Front/front-challenge/pages/login/index.tsx
+++ Front/front-challenge/pages/login/index.tsx
@@ -1,26 +1,84 @@
+import { useState } from "react";
 import { Layout } from "../../components/Layout";
 import Image from "next/image";
 import inicial from "../assets/inicial.svg";
 import Button2 from "../../components/Button2";
 import Input from "../../components/input";
+import { useRouter } from "next/router";
+import axios from "axios";
+
 
 const Home = () => {
-	return (
-		<Layout title={"Home"}>
-			<div className="flex h-full w-full flex-1 flex-col items-center bg-black text-center">
-				<div className="mt-[50%] w-full px-6">
-					<div className=" text-md mx-auto px-10 text-center text-[#fff]">
-						<h3 className="text-lg font-bold">Acesse sua conta</h3>
-					</div>
-					<Input type="e-mail" placeholder="Email" />
-					<Input type="password" placeholder="Senha" />
-				</div>
-				<div className="mt-10 w-full px-10">
-					<Button2 text="Entrar" />
-				</div>
-			</div>
-		</Layout>
-	);
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+
+
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleRegister = () => {
+    const data = {
+      email,
+      password,
+    };
+    console.log(data)
+    axios.post('http://localhost:3000/login', data)
+      .then(response => {
+        alert('Login realizado com sucesso!');
+		// Após a autenticação bem sucedida:
+		localStorage.setItem("username", email);
+		handleRedirect
+      })
+      .catch(error => {
+        console.error(error);
+        alert('Ocorreu um erro durante o registro.');
+      });
+  };
+
+  
+  
+  const handleRedirect = () => {
+    router.push("../home");
+  };
+
+  return (
+    <Layout title={"Home"}>
+      <div className="flex h-full w-full flex-1 flex-col items-center bg-black text-center">
+        <div className="mt-[50%] w-full px-6">
+          <div className=" text-md mx-auto px-10 text-center text-[#fff]">
+            <h3 className="text-lg font-bold">Acesse sua conta</h3>
+          </div>
+          <Input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            name="pass"
+            type="password"
+            placeholder="Senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {error && <p className="text-red-500">{error}</p>}
+        </div>
+        <div className="mt-10 w-full px-10">
+		<Button2 text="Entrar" backgroundColor="bg-[#468C19]" onClick={handleRegister} />
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
